16e2e88ab0b7d9de7e065675fa55eccb9ec79443
islam <islam@C010422882.zipangu.cac.co.jp>
2026-04-16 10:53:47 +0900

    2-1,2-2完了

--- java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -11,6 +13,16 @@ public class Question02_2 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("商品名と値段を教えてください。");
+		String itemName = reader.readLine();
+		String input_num = reader.readLine();
+
+		int itemPrice = Integer.parseInt(input_num);
+
+		System.out.println("商品の名前は" + itemName + "です。");
+		System.out.println("商品の値段は" + itemPrice + "円です。");
 
 	}
 }
