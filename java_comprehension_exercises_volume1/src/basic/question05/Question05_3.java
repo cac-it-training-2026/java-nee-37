34ee561eb0b4e654ef562c27059591665c523313
islam <islam@C010422882.zipangu.cac.co.jp>
2026-04-16 12:18:24 +0900

    クエスチョン５完了

--- java_comprehension_exercises_volume1/src/basic/question05/Question05_3.java
+++ java_comprehension_exercises_volume1/src/basic/question05/Question05_3.java
@@ -1,6 +1,8 @@
 package basic.question05;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第8章 条件分岐
@@ -11,6 +13,34 @@ public class Question05_3 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+		System.out.println("曜日を入力してください。");
+		String userDayInputString = reader.readLine();
+		switch (userDayInputString) {
+		case "月曜日":
+			System.out.println("Monday");
+			break;
+		case "火曜日":
+			System.out.println("Tuesday");
+			break;
+		case "水曜日":
+			System.out.println("Wednesdy");
+			break;
+		case "木曜日":
+			System.out.println("Thursday");
+			break;
+		case "金曜日":
+			System.out.println("Friday");
+			break;
+		case "土曜日":
+			System.out.println("Saturday");
+			break;
+		case "日曜日":
+			System.out.println("Sunday");
+			break;
+		default:
+			break;
+		}
 	}
 }
